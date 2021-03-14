b6a9a4c445bf469e3a935f8660515b5099f15c48
spytni1212 <avdeenkoartem1212@gmail.com>
2021-03-14 21:56:19 +0300

    feat: add local state in status component (71)

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from '../../common/preloader/Preloader'
+import ProfileStatus from './ProfileStatus/ProfileStatus';
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -9,12 +10,12 @@ const ProfileInfo = (props) => {
   return (
   
     <div>
-      <div>
+      {/* <div>
         <img className={s.img} src="https://www.mirf.ru/wp-content/uploads/2020/10/Jujutsu-Kaisen-2.jpg" alt='/ '></img>
-      </div>
+      </div> */}
       <div className={s.descriptionBlock}>
         <img src={props.profile.photos.large} alt='/' />
-        icon + description
+        <ProfileStatus status={'hi how are you'}/>
       </div>
     </div>
   );
